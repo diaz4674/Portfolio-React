c2f1bcf1e079d9cffce7abaeb870cb747b9b73a1
Miguel Diaz <diaz4674@gmail.com>
2020-01-29 15:35:15 -0800

    updates

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,18 +1,17 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
-import { Link } from "react-router-dom";
 
 class Topbar extends React.Component {
   render() {
     return (
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand to="/">Miguel Diaz</Navbar.Brand>
+        <Navbar.Brand href="/">Miguel Diaz</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
-            <Link to="/">About Me</Link>
-            <Link to="/techstack">Tech Stack</Link>
-            <Link to="/projects">Projects</Link>
+            <Nav.Link href="/">About Me</Nav.Link>
+            <Nav.Link href="/techstack">Tech Stack</Nav.Link>
+            <Nav.Link href="/projects">Projects</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
