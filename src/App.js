50c632ac3193b34aeda85bce1d92cd2ff235709d
Miguel Diaz <diaz4674@gmail.com>
2020-01-29 11:30:13 -0800

    Navbar boilerplate done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./styles.css";
 import Landing from "./components/Landing";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import Navbar_Portfolio from "./components/Navbar_Portfolio";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
+          <Navbar_Portfolio />
           <Route exact path="/" component={Landing} />
         </Switch>
       </Router>
