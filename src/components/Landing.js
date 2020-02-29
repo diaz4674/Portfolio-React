70c8ff332cfc16f48111422645ad54982acff3b0
Miguel Diaz <diaz4674@gmail.com>
2020-02-29 01:18:49 -0800

    fixed position

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -15,9 +15,9 @@ class Landing extends Component {
   render() {
     return (
       <Body>
-        <Head>
-          <Video1 style={Vid1} />
-        </Head>
+        {/* <Head> */}
+        <Video1 style={Vid1} />
+        {/* </Head> */}
         <AboutMe>
           <AboutText>
             <Title>About Me</Title>
@@ -43,100 +43,32 @@ const Body = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  height: 100%;
+  position: relative;
+  width: 100%;
 `;
 
 const Head = styled.div`
   height: auto;
-  display: flex;
+  display: block;
   align-items: center;
   justify-content: center;
+  width: 100%;
+  position: relative;
+  background-color: red;
   /* height: 100vh;
 
   @media (max-width: 1425px) {
     height: auto;
   } */
 `;
-// const Batman = styled.img`
-//   z-index: -1;
-//   filter: grayscale(80%);
-//   width: 100%;
-//   overflow: hidden;
-//   transition: 0.5s ease;
-//   @media (max-width: 1650px) {
-//     transition: 0.5s ease;
-//   }
-// `;
-
-// const ProfileDiv = styled.div`
-//   z-index: 1;
-//   top: 131px;
-//   position: absolute;
-//   display: flex;
-//   flex-direction: column;
-//   align-items: center;
-//   transition: 0.1s ease;
-//   @media (max-width: 900px) {
-//     transition: 0.1s ease;
-//     top: 90px;
-//   }
-// `;
-// const Name = styled.p`
-//   font-size: 100px;
-//   color: white;
-//   font-family: "Bangers", cursive;
-//   -webkit-text-stroke: 2px black;
-//   text-transform: uppercase;
-//   transition: 1s ease;
-//   margin: 0;
-//   padding: 0;
-//   @media only screen and (max-width: 1650px) {
-//     transition: 1s ease;
-//     font-size: 80px;
-//   }
-//   @media (max-width: 900px) {
-//     transition: 1s ease;
-//     font-size: 60px;
-//   }
-
-//   @media (max-width: 600px) {
-//     transition: 1s ease;
-//     font-size: 40px;
-//   }
-// `;
-
-// const Name2 = styled.p`
-//   font-size: 120px;
-//   color: white;
-//   font-family: "Bangers", cursive;
-//   -webkit-text-stroke: 2px black;
-//   text-transform: uppercase;
-//   transition: 1s ease;
-//   line-height: 0.1;
-//   margin: 0;
-//   padding: 0;
-//   @media only screen and (max-width: 1650px) {
-//     transition: 1s ease;
-//     font-size: 80px;
-//   }
-//   @media (max-width: 900px) {
-//     transition: 1s ease;
-//     font-size: 60px;
-//     /* nested rules work as expected */
-//   }
-
-//   @media (max-width: 600px) {
-//     transition: 1s ease;
-//     font-size: 40px;
-//     /* nested rules work as expected */
-//   }
-// `;
 
 const AboutMe = styled.div`
   height: 30em;
   background-color: #feffe4;
   display: flex;
   justify-content: space-between;
-
+  top: auto;
   @media (max-width: 1030px) {
     flex-direction: column;
     height: auto;
--- src/components/Video1.js
+++ src/components/Video1.js
@@ -33,30 +33,23 @@ class Video1 extends React.Component {
 
   render = () => {
     const StartVideo = {
-      height: "90vh",
-      position: "absolute",
+      // height: "90vh",
+      position: "relative",
       zIndex: this.state.video1Finished ? "0" : "2",
-      opacity: this.state.video1Finished ? "0" : "1"
+      opacity: this.state.video1Finished ? "0" : "1",
+      width: "100%"
     };
 
     const Video2 = {
-      height: "90vh",
+      // height: "90vh",
       position: "absolute",
       zIndex: this.state.video1Finished ? "2" : "0",
-      opacity: this.state.video1Finished ? "1" : "0"
+      opacity: this.state.video1Finished ? "1" : "0",
+      width: "100%"
     };
 
     return (
       <Container>
-        <video
-          style={StartVideo}
-          ref="vidRef"
-          onEnded={this.switchVideo}
-          src={VideoOne}
-          type="video/mp4"
-          autoPlay
-          muted
-        />
         <video
           style={Video2}
           ref="vidRef"
@@ -67,13 +60,24 @@ class Video1 extends React.Component {
           autoPlay
           muted
         />
+
+        <video
+          style={StartVideo}
+          ref="vidRef"
+          onEnded={this.switchVideo}
+          src={VideoOne}
+          type="video/mp4"
+          autoPlay
+          muted
+        />
       </Container>
     );
   };
 }
 
 const Container = styled.div`
-  height: auto;
+  /* height: 90vh; */
+  position: relative;
   width: 100%;
 `;
 
