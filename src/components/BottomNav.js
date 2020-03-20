74b21881e163d82a36ff1a0e00257f31303a8ccc
Miguel Diaz <diaz4674@gmail.com>
2020-03-19 23:18:56 -0700

    Fixed z-index issues and competing CSS's

--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -3,25 +3,9 @@ import styled from "styled-components";
 import Hamburger from "../icons/Hamburger.png";
 
 class BottomNav extends React.Component {
-  // constructor() {
-  //   super();
-  //   // this.state = {
-  //   //   isHovered: false
-  //   // };
-  // }
-
-  // handleHover() {
-  //   this.setState({
-  //     isHovered: !this.state.isHovered
-  //   });
-  // }
   render() {
     return (
-      <Container
-      // onMouseEnter={() => this.handleHover()}
-      // onMouseLeave={() => this.handleHover()}
-      >
-        {/* {this.state.isHovered ? ( */}
+      <>
         <div className="slide this">
           <Text href="#Home"> Home </Text>
           <Text href="#About_me"> About </Text>
@@ -33,29 +17,10 @@ class BottomNav extends React.Component {
             className="hamburger"
           />
         </div>
-        {/* ) : ( */}
-        {/* <HamburgerDiv>
-          <HamburgerIcon src={Hamburger} />
-        </HamburgerDiv>
-        )} */}
-      </Container>
+      </>
     );
   }
 }
-
-const Container = styled.div`
-  display: flex;
-  -webkit-box-align: center;
-  align-items: center;
-  -webkit-box-pack: end;
-  justify-content: flex-end;
-  box-shadow: rgba(0, 0, 0, 0.2) -5px -5px 10px;
-  position: fixed;
-  right: 0px;
-  bottom: 0px;
-  margin-bottom: 25px;
-  overflow: hidden;
-`;
 
 const Text = styled.a`
   display: flex;
--- src/components/Projects/Carousel.js
+++ src/components/Projects/Carousel.js
@@ -6,10 +6,32 @@ import Test from "../LandingVideos/vid2.mp4";
 
 // Projects Card Container that is a react-bootstrap Carousel
 class Projects extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleSelect = this.handleSelect.bind(this);
+
+    this.state = {
+      index: 0,
+      direction: null
+    };
+  }
+
+  handleSelect(selectedIndex, e) {
+    this.setState({
+      index: selectedIndex,
+      direction: e.direction
+    });
+  }
   render() {
+    const { index, direction } = this.state;
     return (
       <Container>
-        <Carousel indicators={false} interval={false} data-interval={false}>
+        <Carousel
+          activeIndex={index}
+          direction={direction}
+          onSelect={this.handleSelect}
+        >
           <Carousel.Item
             interval={false}
             data-interval={false}
@@ -27,6 +49,34 @@ class Projects extends React.Component {
                 playsInline
                 muted
                 controls
+              />
+              <Description
+                description="A site where users may store their favorite sites all
+              in one place, and browse by sections to view their saved sites."
+                tech=" React | React Hooks | Redux | CSS3 | Material-UI Framework
+              | Express | SQLite3 | Axios | Bcryptjs "
+                link="https://quizzical-blackwell-d608a3.netlify.com/"
+                gitrepo="https://github.com/diaz4674/BookMark"
+              />
+            </VideoContainer>
+          </Carousel.Item>
+          <Carousel.Item
+            interval={false}
+            data-interval={false}
+            className="carousel"
+          >
+            <VideoContainer>
+              <Title> Bookmark </Title>
+              <Video
+                ref="vidRef"
+                onEnded={this.switchVideo}
+                src={Test}
+                type="video/mp4"
+                loop
+                autoPlay
+                playsInline
+                muted
+                // controls
               />
               <Description
                 description="A site where users may store their favorite sites all in one place, and browse by sections to view their saved sites."
@@ -53,34 +103,7 @@ class Projects extends React.Component {
                 autoPlay
                 playsInline
                 muted
-                controls
-              />
-              <Description
-                description="A site where users may store their favorite sites all in one place, and browse by sections to view their saved sites."
-                tech=" React | React Hooks | Redux | CSS3 | Material-UI Framework |
-                Express | SQLite3 | Axios | Bcryptjs "
-                link="https://quizzical-blackwell-d608a3.netlify.com/"
-                gitrepo="https://github.com/diaz4674/BookMark"
-              />
-            </VideoContainer>
-          </Carousel.Item>
-          <Carousel.Item
-            interval={false}
-            data-interval={false}
-            className="carousel"
-          >
-            <VideoContainer>
-              <Title> Bookmark </Title>
-              <Video
-                ref="vidRef"
-                onEnded={this.switchVideo}
-                src={Test}
-                type="video/mp4"
-                loop
-                autoPlay
-                playsInline
-                muted
-                controls
+                // controls
               />
               <Description
                 description="A site where users may store their favorite sites all in one place, and browse by sections to view their saved sites."
