a8300e6cafa47ab48ea9ae5fa4bcb2bdae7534be
Miguel Diaz <diaz4674@gmail.com>
2020-03-20 22:05:24 -0700

    Updated video position

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -16,36 +16,6 @@ const Vid1 = {
 };
 
 class Landing extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   // Don't call this.setState() here!
-  //   this.state = { prevScrollpos: window.pageYOffset, visible: true };
-  //   this.handleClick = this.handleClick.bind(this);
-  // }
-
-  // // Adds an event listener when the component is mount.
-  // componentDidMount() {
-  //   window.addEventListener("scroll", this.handleScroll);
-  // }
-
-  // // Remove the event listener when the component is unmount.
-  // componentWillUnmount() {
-  //   window.removeEventListener("scroll", this.handleScroll);
-  // }
-
-  // // Hide or show the menu.
-  // handleScroll = () => {
-  //   const { prevScrollpos } = this.state;
-
-  //   const currentScrollPos = window.pageYOffset;
-  //   const visible = prevScrollpos > currentScrollPos;
-
-  //   this.setState({
-  //     prevScrollpos: currentScrollPos,
-  //     visible
-  //   });
-  // };
-
   render() {
     return (
       <>
