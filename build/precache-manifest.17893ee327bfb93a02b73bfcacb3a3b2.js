473d9ec50532735337bc16b85aeeecf26658e108
miguel diaz <diaz4674@gmail.com>
2020-08-25 14:31:07 -0700

    redirects file

---
+++ build/precache-manifest.17893ee327bfb93a02b73bfcacb3a3b2.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9812b4bf2c3e165f792c40b79440a9e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "018a464c61cadab2b661",
+    "url": "/static/css/2.61a12f80.chunk.css"
+  },
+  {
+    "revision": "892320e3cd6dd39539e2",
+    "url": "/static/css/main.0b6ccd81.chunk.css"
+  },
+  {
+    "revision": "018a464c61cadab2b661",
+    "url": "/static/js/2.acfaa56d.chunk.js"
+  },
+  {
+    "revision": "683bca883f97b7842203623227163443",
+    "url": "/static/js/2.acfaa56d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "892320e3cd6dd39539e2",
+    "url": "/static/js/main.896b185d.chunk.js"
+  },
+  {
+    "revision": "58cb158aa2d546ca4898",
+    "url": "/static/js/runtime-main.33f659d6.js"
+  },
+  {
+    "revision": "9a59ff1e021feaf65e06c46ed6071733",
+    "url": "/static/media/Alabastra.9a59ff1e.mp4"
+  },
+  {
+    "revision": "2c892659c2e4cdde737474d05da026ee",
+    "url": "/static/media/BatmanLego.2c892659.jpg"
+  },
+  {
+    "revision": "b5faa27457b6e16271b7a40d02563d2f",
+    "url": "/static/media/CSS3.b5faa274.png"
+  },
+  {
+    "revision": "454a400ca987d4271e54045982ea3876",
+    "url": "/static/media/HTMLLogo.454a400c.png"
+  },
+  {
+    "revision": "79b18a5d205cdebc264fc06817b73584",
+    "url": "/static/media/Hamburger.79b18a5d.png"
+  },
+  {
+    "revision": "3d3ed003d5b3f8a73e66eb82433cd1e6",
+    "url": "/static/media/JS.3d3ed003.png"
+  },
+  {
+    "revision": "a6bbb26519e919a8a66791541895e0ea",
+    "url": "/static/media/Ledgerfire.a6bbb265.mp4"
+  },
+  {
+    "revision": "f9df6b4e18274f8c8388523fbee331f9",
+    "url": "/static/media/Liftquest.f9df6b4e.mp4"
+  },
+  {
+    "revision": "37fe8322b169ddbdeabf75930e886ac6",
+    "url": "/static/media/React.37fe8322.png"
+  },
+  {
+    "revision": "c6ba7879fd2edbcc4180b76f72fe7c8d",
+    "url": "/static/media/SAM.c6ba7879.mp4"
+  },
+  {
+    "revision": "c8fb1aa91cabd5e208b1ad368579a609",
+    "url": "/static/media/adobeCreative.c8fb1aa9.png"
+  },
+  {
+    "revision": "a4cfc45fdb43b75c191cb9a0b75ab5d7",
+    "url": "/static/media/arrow.a4cfc45f.png"
+  },
+  {
+    "revision": "fbd5283bd7408d040c555c86baa1b650",
+    "url": "/static/media/bookmarkDemo.fbd5283b.mp4"
+  },
+  {
+    "revision": "64876c3e471914c1391ddc7b2e7ec3dc",
+    "url": "/static/media/mail.64876c3e.png"
+  },
+  {
+    "revision": "580117f19ce155242665fcb34fed4991",
+    "url": "/static/media/profile.580117f1.jpg"
+  },
+  {
+    "revision": "067050f6ecc418ea7a7d2f3557683c8c",
+    "url": "/static/media/python.067050f6.png"
+  },
+  {
+    "revision": "ad8f3dec917ac4a2301dcfbc24c15fa9",
+    "url": "/static/media/us4.0.ad8f3dec.png"
+  },
+  {
+    "revision": "cbfcead7d2e17158f8c286db5618dea6",
+    "url": "/static/media/vid1.cbfcead7.mp4"
+  },
+  {
+    "revision": "8b1544bbfeb76de49abb3600a65d5113",
+    "url": "/static/media/vid2.8b1544bb.mp4"
+  }
+]);
